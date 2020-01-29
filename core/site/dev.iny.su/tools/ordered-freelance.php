4d36d2529f283f12a3c627937032af53c284dc5f
yesfedor <yesfedor.go@gmail.com>
2020-01-29 22:31:32 +0400

    master

--- core/site/dev.iny.su/tools/ordered-freelance.php
+++ core/site/dev.iny.su/tools/ordered-freelance.php
@@ -18,34 +18,10 @@ $ordered_freelance_data = dbGetAll($queryOrdered, $varOrdered);
     </div>
 </div>
 <script>
-let ordered_freelance_data = <?= json_encode($ordered_freelance_data) ?>
+init.js.add('ordered_freelance_render', 'module/ordered-freelance.js', 1);
+toastr.info('Загрузка заказов')
+setTimeout(() => {
+    ordered_freelance_render('#ordered_freelance_container', <?= json_encode($ordered_freelance_data) ?>)
+}, 1000);
 
-function ordered_freelance_cards(cards) {
-    return `
-        <div class="col-12 border py-2 mb-2">
-            <h4 class="d-block theme-title mb-2 py-0">` + cards.name + ` <small class="theme-text">` + cards.mail + `, ` + cards.number + `</small> (<small data-ctime="` + cards.time + `" class="theme-text"></small>)</h4>
-            <h5 class="d-block theme-text mb-2 py-0">
-                <span class="d-block mb-1 py-0">Описание</span>
-                ` + cards.description + `
-            </h5>
-            <h5 class="d-block theme-text mb-2 py-0">
-                <span class="d-block mb-1 py-0">Задача</span>
-                ` + cards.task + `
-            </h5>
-            <h5 class="d-block theme-text mb-2 py-0">
-                <span class="">Бюджет:</span> ` + cards.budget + `
-            </h5>
-        </div>
-    `
-}
-function ordered_freelance_render(el, data) {
-    let html = ``
-    for(let i = 0; i < data.length;) {
-        html = html + ordered_freelance_cards(data[i])
-        i++
-    }
-    $(el).prepend(html);
-}
-
-ordered_freelance_render('#ordered_freelance_container', ordered_freelance_data)
 </script>
---
+++ public_html/web/js/module/ordered-freelance.js
@@ -0,0 +1,27 @@
+function ordered_freelance_cards(cards) {
+    return `
+        <div class="col-12 border py-2 mb-2">
+            <h3 class="d-block theme-title border-bottom pb-2 mb-3"><span class="text-primary">Имя:</span> ` + cards.name + `</h3>
+            <h4 class="d-block theme-title border-bottom pb-2 mb-3"><span class="text-primary">Почта:</span> ` + cards.mail + `</h4>
+            <h4 class="d-block theme-title border-bottom pb-2 mb-3"><span class="text-primary">Номер:</span> ` + cards.number + `</h4>
+            <h4 class="d-block theme-title border-bottom pb-2 mb-3"><span class="text-primary">Дата:</span> <span data-ctime="` + cards.time + `"></span></h4>
+            <h5 class="d-block theme-text border-bottom pb-2 mb-3">
+                <span class="text-primary mb-1 py-0">Описание:</span> ` + cards.description + `
+            </h5>
+            <h5 class="d-block theme-text border-bottom pb-2 mb-3">
+                <span class="text-primary mb-1 py-0">Задача:</span> ` + cards.task + `
+            </h5>
+            <h5 class="d-block theme-text">
+                <span class="text-primary">Бюджет:</span> ` + cards.budget + `
+            </h5>
+        </div>
+    `
+}
+function ordered_freelance_render(el, data) {
+    let html = ``
+    for(let i = 0; i < data.length;) {
+        html = html + ordered_freelance_cards(data[i])
+        i++
+    }
+    $(el).prepend(html);
+}
