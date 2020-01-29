c7a9e15b2751dc3260c9623bba82c5cc61cc4185
yesfedor <yesfedor.go@gmail.com>
2020-01-29 13:38:40 +0400

    master

--- core/template/html.tpl.php
+++ core/template/html.tpl.php
@@ -40,10 +40,7 @@ $html_comment_block = '<!-- '.PHP_EOL.' ADMIN: https://vk.com/yesfedor '.PHP_EOL
             info: <?= json_encode($domainInfo[appGetDomain()], JSON_UNESCAPED_UNICODE) ?>,
             user: {auth: <?= ($_SESSION['user']['uid'] ? 'true':'false') ?>,  uid: '<?= $_SESSION['user']['uid'] ?>', url: '<?= $_SESSION['user']['url'] ?>', name: '<?= $_SESSION['user']['name'] ?>', surname: '<?= $_SESSION['user']['surname'] ?>', gender: '<?= $_SESSION['user']['gender'] ?>', access: '<?= $_SESSION['user']['access'] ?>', mail_verfy: '<?= $_SESSION['user']['mail_verfy'] ?>', number_verfy: '<?= $_SESSION['user']['number_verfy'] ?>'},
             hash: {appRoot:'<?= getAppRootHash() ?>', logout: '<?= $hash['logout'] ?>'},
-            themeVersion: {
-                light: 64,
-                dark: 128
-            }
+            themeVersion: {light: 64, dark: 133}
         }
         let servers = {
             api: '',
