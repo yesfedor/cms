bc8abc4b3d3000ae75412af26a41ff6f87320a9c
yesfedor <yesfedor.go@gmail.com>
2020-02-06 23:15:48 +0400

    master

--- core/engine/db.php
+++ core/engine/db.php
@@ -1,6 +1,9 @@
 <?php
 $lastInsertId = 0;
 
+if (!$_SESSION['db_all_request']) $_SESSION['db_all_request'] = 0;
+if ($_SESSION['db_all_request'] > 1000) $_SESSION['db_all_request'] = 0;
+
 function dbFetchAll($result) { 	
    	return $result = $result->fetchAll(PDO::FETCH_ASSOC);
 }
@@ -9,6 +12,8 @@ function dbFetchOne($result) {
 }
 function dbGetAll($query, $var) {
     global $db;
+    global $_SESSION;
+    $_SESSION['db_all_request']++;
     $stmt = $db->prepare($query);
     $stmt->execute($var);
     $result = dbFetchAll($stmt);
@@ -23,6 +28,8 @@ function dbGetOne($query, $var) {
 function dbAddOne($query, $var) {
     global $db;
     global $lastInsertId;
+    global $_SESSION;
+    $_SESSION['db_all_request']++;
     $db->prepare($query)->execute($var);
     $db_lastInsertId = $db->lastInsertId();
     $lastInsertId = $db_lastInsertId;
--- public_html/api/0.1/session.php
+++ public_html/api/0.1/session.php
@@ -18,12 +18,15 @@ if ($_GET['act'] == 'delete' and $_GET['object']) {
         case 'user.users':
             $_SESSION['users'] = [];
         break;
+        case 'db.all_request':
+            $_SESSION['db_all_request'] = 0;
+        break;
     }
+    echo '<meta http-equiv="refresh" content="0; url=/api.php?_action=session&v=0.1">';
 }
 ?>
+<link rel="stylesheet" href="style.css">
 <h4>
-Delete: <a href="?act=delete&object=all">{all}</a>, <a href="?act=delete&object=user.user">{user.user}</a>, <a href="?act=delete&object=user.users">{user.users}</a>
+Delete: <a href="/api.php?_action=session&v=0.1&act=delete&object=all">{all}</a>, <a href="/api.php?_action=session&v=0.1&act=delete&object=user.user">{user.user}</a>, <a href="/api.php?_action=session&v=0.1&act=delete&object=user.users">{user.users}</a>, <a href="/api.php?_action=session&v=0.1&act=delete&object=db.all_request">{db.all_request}</a>
 </h4>
-<h4>
 <?= debug($_SESSION) ?>
-</h4>
