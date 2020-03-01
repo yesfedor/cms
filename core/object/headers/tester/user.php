f6dd7682c77603445703952c71ffa37e4c36baf2
yesfedor <yesfedor.go@gmail.com>
2020-03-01 17:21:16 +0400

    master

--- core/object/headers/tester/user.php
+++ core/object/headers/tester/user.php
@@ -2,7 +2,7 @@
     <div class="container">
         <a href="/review" onclick="return nav.go(this);" class="navbar-brand"><b class="theme-logo"><?= $logo ?></b></a>
         <ul class="d-none d-lg-block navbar-nav mr-auto">
-            <!--<li class="nav-item"><?= url('/info', 'Как пользоваться?', false, 'theme-nav-link nav-link waves-effect waves-light', true) ?></li>-->
+            <li class="nav-item"><?= url('/info', 'Полезная информация', false, 'theme-nav-link nav-link waves-effect waves-light', true) ?></li>
         </ul>
         <ul class="d-none d-lg-block navbar-nav ml-auto">
             <li class="nav-item text-right">
--- core/site/tester.iny.su/main/info.php
+++ core/site/tester.iny.su/main/info.php
@@ -3,8 +3,40 @@ $warp = 'warp';
 ?>
 <div class="container">
     <div class="row">
-        <div class="col-12 white border border-primary text-center my-5 py-5">
-            <h1>Info Page</h1>
+        <div class="col-12 text-center my-3 py-3">
+            <div class="row">
+                <div class="col-12 mb-3">
+                    <h3 class="black-text my-0">Краткое руководство и лимиты в тестере</h3>
+                </div>
+                <div class="col-12 mb-3">
+                    <div class="row">
+                        <div class="col-10 offset-1 mb-3">
+                            <ul class="list-group mb-3">
+                                <li id="about-review" class="list-group-item theme-header theme-nav-link">Главная</li>
+                                <li class="list-group-item">Рекомендуемые тесты начнут отображаться после 60 дней активного использования тестера, однако это может произойти раньше, если Вас пригласит автор закрытого теста</li>
+                                <li class="list-group-item">В блоке "Новые тесты" показывается не больше 6 недавно созданных тестов</li>
+                                <li class="list-group-item">Блок "Мои тесты" показывает не все Ваши тесты, а только 6 последних</li>
+                            </ul>
+                            <ul class="list-group mb-3">
+                                <li id="about-create" class="list-group-item theme-header theme-nav-link">В процессе создания теста</li>
+                                <li class="list-group-item">Название теста обязательно для заполнения, максимальная длина названия теста - <code>256 сим.</code></li>
+                                <li class="list-group-item">Поля "Описание" и "Необходимое для выполнения" допустимо оставить пустыми, максимальная длина полей - <code>2048 сим.</code></li>
+                                <li class="list-group-item">Заголовок вопроса - содержит главный вопрос, либо сам вопрос, если он не превышает максимальной длины - <code>512 сим.</code></li>
+                                <li class="list-group-item">Пояснение вопроса - сожержит тело вопроса или сам вопрос, максимальная длина - <code>2048 сим.</code></li>
+                                <li class="list-group-item">Объяснение вопроса - может быть пустым, либо содержать как краткое объяснение, так и подробное. Максимальная длина - <code>2048 сим.</code></li>
+                                <li class="list-group-item">Подсказка к вопросу - может содержать, например "дайте ответ в кг" - либо округление числа то кого-то разряда. Может иметь общий вид "Введите ответ". максимальная длина - <code>От 64 до 512 сим.</code></li>
+                                <li class="list-group-item">Ответ - содежит правильный овтет на вопрос, либо номера правильного(ых) пунктов максимальная длина - <code>512 сим.</code></li>
+                            </ul>
+                            <ul class="list-group mb-3">
+                                <li id="about-test" class="list-group-item theme-header theme-nav-link">Решение теста</li>
+                                <li class="list-group-item">Здесь Вы можете решить созданный Вами, либо кем-то другим тест</li>
+                                <li class="list-group-item">Если Вы являетесь автором теста, то сможете посмотреть его решения, также Вам будут приходить уведомления о новых решениях</li>
+                                <li class="list-group-item">Удаляя тест <a onclick="return nav.away(this);" href="https://iny.su/admin">Администрация сайта</a> не сможет его восстановить, также модераторы имеют право удалить тест БЕЗ Вашего согласия!</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
