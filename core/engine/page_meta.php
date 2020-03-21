eb047390fb4a04a1e9c1fec0ac4cf682a4dfca5a
yesfedor <yesfedor.go@gmail.com>
2020-03-21 18:34:00 +0400

    master

--- core/engine/page_meta.php
+++ core/engine/page_meta.php
@@ -45,6 +45,12 @@ if ($domainBase[appGetDomain()] == 'tester') {
 if ($domainBase[appGetDomain()] == 'go' and $url_page != 'main' and $url_p1 != 'info') {
     header('Location: '.$p['data']['url_to']);
     echo '<meta http-equiv="refresh" content="0; url='.$p['data']['url_to'].'">';
+
+    $query_go_cc_add_click = "UPDATE go_cc SET `click` = `click` + 1 WHERE `id` = :id";
+    $var_go_cc_add_click = [
+        ':id' =>  $p['data']['id']
+    ];
+    dbAddOne($query_go_cc_add_click, $var_go_cc_add_click);
 }
 
 ?>
