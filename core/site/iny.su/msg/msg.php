75d7f7fcf9d16c295ee1276fb1f165eed9fd4cfa
yesfedor <yesfedor.go@gmail.com>
2020-03-27 22:28:50 +0400

    master

--- core/site/iny.su/msg/msg.php
+++ core/site/iny.su/msg/msg.php
@@ -3,14 +3,6 @@ $ui = true;
 $warp = "warp-page";
 $pageActive['msg'] = 'active';
 ?>
-<style>
-.h90vh {
-    min-height: 90vh;
-}
-.h80vh {
-    min-height: 80vh;
-}
-</style>
 <div class="col-12 border border-primary rounded">
     <div class="row">
         <div class="col-12 text-center px-0">
@@ -18,7 +10,7 @@ $pageActive['msg'] = 'active';
             <hr class="w-100 border-primary my-3 py-0">
         </div>
         <div class="col-12">
-            <div class="row h80vh justify-content-center align-items-center">
+            <div class="row h70vh justify-content-center align-items-center">
                 <div class="col-10 text-center">
                     <h4 class="text-muted my-0 py-0">Когда вы начнете переписку, она появится здесь</h4>
                 </div>
