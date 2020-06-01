d858e9bd53c5dcc83a2c71028caf07dabc487ba7
yesfedor <yesfedor.go@gmail.com>
2020-06-01 17:15:47 +0400

    master

--- core/engine/page_meta.php
+++ core/engine/page_meta.php
@@ -1,8 +1,5 @@
 <?php
-/* 
- * @comment условия для показа meta-данных
- * подключается файл global/meta.php у сайта
-*/
+// подключается файл global/meta.php у сайта
 $enginePageMetaSite = $thisSitePath.'global/meta.php';
 include_once($enginePageMetaSite);
 ?>
