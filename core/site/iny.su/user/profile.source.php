34a44fdc80e80833743455abe506be3b49af381a
yesfedor <yesfedor.go@gmail.com>
2020-06-06 22:39:29 +0400

    master

--- core/site/iny.su/user/profile.source.php
+++ core/site/iny.su/user/profile.source.php
@@ -4,10 +4,10 @@ $sourceData['content'] = '
 <div class="col-12 mt-1">
     <div class="row my-0 py-0">
         <div class="col-12 text-center">
-            <div class="h5 black-text">View account</div>
+            <h6 class="theme-title my-0 py-2">Пользователь</h6>
         </div>
-        <div class="col-12 text-center"><i class="fas fa-'.$p_source['data']['gender'].' fa-2x text-primary"></i></div>
-        <div class="col-12 text-center"><h6 class="h6 black-text text-truncate" style="cursor: pointer;" onclick="nav.router(\'/id'.$p_source['data']['uid'].'\', true)">'.$p_source['data']['name'].' '.$p_source['data']['surname'].'</h6></div>
+        <div class="col-12 text-center"><i class="fas fa-'.$p_source['data']['gender'].' fa-2x theme-icon"></i></div>
+        <div class="col-12 text-center"><h6 class="h6 theme-title text-truncate" style="cursor: pointer;" onclick="nav.router(\'/id'.$p_source['data']['uid'].'\', true)">'.$p_source['data']['name'].' '.$p_source['data']['surname'].'</h6></div>
     </div>
 </div>
 ';
