8454dad72873cacfef5ef2edf41e57df637a5e59
yesfedor <yesfedor.go@gmail.com>
2020-06-30 01:49:48 +0400

    master

--- public_html/api/0.1/checker.php
+++ public_html/api/0.1/checker.php
@@ -15,7 +15,7 @@ $bigData = [
     'notice' => ($notice ? $notice : false),
     'msg' => false
 ];
-
+sleep($serverTimeout);
 //return
 $json_v1 = $public.'/api/0.1/json.v1.php';
 include_once($json_v1);
