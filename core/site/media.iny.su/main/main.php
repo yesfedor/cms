a9f3cf1639a67f0c2b1664ed1b2ecd8dd4b52a2f
yesfedor <yesfedor.go@gmail.com>
2020-07-18 02:52:26 +0400

    master

---
+++ core/site/media.iny.su/main/main.php
@@ -0,0 +1,11 @@
+<?php
+$ui = false;
+$warp = "warp";
+?>
+<div class="container-fluid">
+    <div class="row justify-content-center align-items-center h100vh">
+        <div class="theme-primary col-10 rounded text-center py-3">
+            <h1 class="theme-title py-3">INY Media</h1>
+        </div>
+    </div>
+</div>
