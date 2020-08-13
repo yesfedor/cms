ea78181e8514bdb2314b46684f5e0a80793f6a94
yesfedor <yesfedor.go@gmail.com>
2020-08-13 18:46:04 +0400

    master

--- core/site/studio.iny.su/main/main.php
+++ core/site/studio.iny.su/main/main.php
@@ -46,38 +46,38 @@ $warp = "warp";
         <div class="col-12 py-3">
             <div class="row">
                 <div class="col-12 text-center mt-5 mb-3">
-                    <h2 class="black-text font-weight-bold">Наши услуги</h2>
+                    <h2 data-aos="fade-up" data-aos-duration="1000" class="black-text font-weight-bold">Наши услуги</h2>
                 </div>
                 <div class="col-12 text-center mt-3 mb-5">
-                    <h3 class="black-text">
+                    <h3 data-aos="fade-up" data-aos-duration="1000" class="black-text">
                         Тут предоставлены готовые предложения.
                         Вы можете сами выбрать необходимые вам услуги.
                     </h3>
                 </div>
-                <div class="container mt-5 pt-5 pb-3 px-5 z-depth-1 white">
+                <div data-aos="fade-up" class="container mt-5 pt-5 pb-3 px-5 z-depth-1 white">
                     <section class="text-center mdb-color-text">
                         <div class="row text-center d-flex justify-content-center my-5">
-                            <div class="col-lg-3 col-md-6 mb-4">
+                            <div data-aos="fade-up" data-aos-duration="1000" class="col-lg-3 col-md-6 mb-4">
                                 <i class="fas fa-user-cog blue-text fa-3x mb-4"></i>
                                 <h5 class="font-weight-normal mb-3">Полная настройка</h5>
                                 <p class="text-muted mb-0"><span class="d-block">Подходит для начала проекта.</span> Мы полностью настраиваем ваш проект + набираем персонал + создаем систему персонала + необходимые локальные акты, регулирующие работу персонала.</p>
                             </div>
-                            <div class="col-lg-3 col-md-6 mb-4">
+                            <div data-aos="fade-up" data-aos-duration="1000" class="col-lg-3 col-md-6 mb-4">
                                 <i class="fas fa-microchip blue-text fa-3x mb-4"></i>
                                 <h5 class="font-weight-normal mb-3">Техническая настройка</h5>
                                 <p class="text-muted mb-0"><span class="d-block">Подходит для проекта с готовым персоналом.</span> Настройка ролей, каналов, иных технических аспектов.</p>
                             </div>
-                            <div class="col-lg-3 col-md-6 mb-4">
+                            <div data-aos="fade-up" data-aos-duration="1000" class="col-lg-3 col-md-6 mb-4">
                                 <i class="fas fa-sitemap blue-text fa-3x mb-4"></i>
                                 <h5 class="font-weight-normal mb-3">Организация работы персонала</h5>
                                 <p class="text-muted mb-0">Набираем персонал + создаем систему персонала + необходимые локальные акты, регулирующие работу персонала.</p>
                             </div>
-                            <div class="col-lg-3 col-md-6 mb-4">
+                            <div data-aos="fade-up" data-aos-duration="1000" class="col-lg-3 col-md-6 mb-4">
                                 <i class="fas fa-satellite blue-text fa-3x mb-4"></i>
                                 <h5 class="font-weight-normal mb-3">Создание сайта</h5>
                                 <p class="text-muted mb-0">Креативим, верстаем, публикуем на хостинг, пишем скрипты, наш разработчик <a onclick="return nav.away(this);" href="https://iny.su/landings/yesfedor?from=studio">@yesfedor</a></p>
                             </div>
-                            <div class="col-12 mb-0">
+                            <div data-aos="fade-up" data-aos-duration="1000" class="col-12 mb-0">
                                 <h3 class="mb-0">
                                     <span class="font-weight-bold mr-3">Вы можете сформировать ваш заказ индивидуально</span>
                                     <a onclick="return nav.away(this);" href="https://vk.com/im?media=&sel=-193422646" class="btn btn-outline-blue btn-large btn-rounded z-depth-0">Написать</a>
@@ -95,25 +95,25 @@ $warp = "warp";
         <div class="col-12 text-center py-3">
             <div class="row">
                 <div class="col-12 text-center my-5">
-                    <h2 class="black-text font-weight-bold">Почему мы?</h2>
+                    <h2 data-aos="fade-down" class="black-text font-weight-bold">Почему мы?</h2>
                 </div>
                 <div class="col-12">
                     <div class="container row mx-auto mb-5">
-                        <div class="col-12 col-lg-4 black-text text-center">
+                        <div data-aos="fade-right" class="col-12 col-lg-4 black-text text-center">
                             <i class="fas fa-check black-text fa-lg mr-3"></i><span class="h4 font-weight-bold">Скорость</span>
                             <p class="d-block text-center">Мы стараемся сделать все максимально <b>быстро</b>!</p>
                         </div>
-                        <div class="col-12 col-lg-4 black-text text-center">
+                        <div data-aos="fade-up" class="col-12 col-lg-4 black-text text-center">
                             <i class="fas fa-check black-text fa-lg mr-3"></i><span class="h4 font-weight-bold">Качество</span>
                             <p class="d-block text-center">Мы уделяем большую часть внимания <b>качеству</b>!</p>
                         </div>
-                        <div class="col-12 col-lg-4 black-text text-center">
+                        <div data-aos="fade-left" class="col-12 col-lg-4 black-text text-center">
                             <i class="fas fa-check black-text fa-lg mr-3"></i><span class="h4 font-weight-bold">Опыт</span>
                             <p class="d-block text-center">Наши сотрудники имеют <b>опыт работы на разных платформах</b>!</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-12 text-center mt-5 mx-0">
+                <div data-aos="fade-up" class="col-12 text-center mt-5 mx-0">
                     <h2 class="h1-responsive black-text mb-5">Если вы действительно чего-то хотите, не ждите — будьте нетерпеливыми.</h2>
                     <h4 class="h2-responsive black-text mt-5"><a onclick="return nav.away(this);" href="https://vk.com/im?media=&sel=-193422646">Напишите нам</a>, и мы вам поможем.</h4>
                     <h4 class="h2-responsive black-text mt-5">vadimnovikov108@gmail.com</h4>
@@ -145,8 +145,16 @@ let studio = {
         })
         this.pageAboutTitle()
     },
+    animate() {
+        init.css.add('aos', 'domain:https://unpkg.com/aos@2.3.1/dist/aos.css', 1)
+        init.js.add('aos', 'domain:https://unpkg.com/aos@2.3.1/dist/aos.js', 1)
+        setTimeout(() => {
+            AOS.init()
+        }, 500)
+    },
     init() {
-        this.pageAbout()
+        this.animate()
+        this.pageAbout()        
     }
 }
 studio.init()
