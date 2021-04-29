58bad7f1956592c5efa9f964be8c822fb3e29a31
yesfedor <yesfedor.go@gmail.com>
2021-04-29 22:34:04 +0400

    Upload

--- core/site/iny.su/main/index.auth_true.php
+++ core/site/iny.su/main/index.auth_true.php
@@ -4,10 +4,19 @@ if ($_SESSION['domain-redirect']) {
     $domainRedirect = $_SESSION['domain-redirect'];
     if ($domainInfo[$domainRedirect]['appname'] and !$_SESSION['url-redirect']) {
         $domainRedirect = $domainInfo[$domainRedirect]['domain'];
+        
+        $jwt_user_data = [
+            'uid' =>  $_SESSION['user']['uid'],
+            'name' => $_SESSION['user']['name'],
+            'surname' => $_SESSION['user']['surname'],
+            'mail' => $_SESSION['user']['mail']
+        ];
+        $jwt_auth = jwt_encode($_SESSION['user']['uid'], $jwt_user_data);
+
         echo '
         <script>
             toastr.info("Domain Redirect..")
-            window.location = "//'.$domainRedirect.'/?auth=true&_origin='.$domainBase['main'].'"
+            window.location = "//'.$domainRedirect.'/?auth='.$jwt_auth.'&_origin='.$domainBase['main'].'"
         </script>
         ';
     } elseif ($_SESSION['url-redirect']) {
